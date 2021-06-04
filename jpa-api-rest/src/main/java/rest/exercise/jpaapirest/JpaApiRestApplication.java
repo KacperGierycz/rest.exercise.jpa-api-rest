c23ffb0d1177820325075a56a94086d133c3b6be
Kacper <kacpergierycz@gmail.com>
2021-06-04 08:20:53 +0200

    Project initialize WEB JPA APACHE data

---
+++ jpa-api-rest/src/main/java/rest/exercise/jpaapirest/JpaApiRestApplication.java
@@ -0,0 +1,13 @@
+package rest.exercise.jpaapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaApiRestApplication.class, args);
+	}
+
+}
---
+++ jpa-api-rest/src/test/java/rest/exercise/jpaapirest/JpaApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.exercise.jpaapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
